44519dbff22b171d1f0fb5bdc0bb5a3a24502306
ChamalJaliya <cjaliya.sln2@gmail.com>
2020-02-09 11:41:04 +0530

    modify forecast details

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -5,19 +5,25 @@ const forecast = (lat, lng, callback) => {
         "https://api.darksky.net/forecast/d9ecd1b743e5cbf3f017d744f512f62b/" +
         lat +
         "," +
-        lng + "?units=si";
+        lng +
+        "?units=si";
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback("Unable to connect to the weather services", undefined);
         } else if (body.error) {
             callback("Unable to fetch the location try again", undefined);
         } else {
+
             callback(
                 undefined,
                 body.daily.data[0].summary +
                 "It's currently " +
                 body.currently.temperature +
-                " degrees out , with probability of " +
+                "°C degrees out ,The high today is a " +
+                body.daily.data[0].temperatureHigh +
+                "°C degrees with a low of " +
+                body.daily.data[0].temperatureLow +
+                "°C degrees with probability of " +
                 body.currently.precipProbability +
                 " %  chance of raining ."
             );
